4e100fffc3c6f43775ed2c19a61c20084e5b6f44
Nir Yarden <nir@rookout.com>
2019-11-07 13:54:37 +0200

    generating final report

--- inputs/auto_collect_repos.py
+++ inputs/auto_collect_repos.py
@@ -138,7 +138,7 @@ def collect_repos():
         langs = json.loads(file_content.read()).keys()
 
     # modify the wished size of list to fit the github api. calc how many response pages needed per language.
-    wished_list_size = 400
+    wished_list_size = 30000
     addition = wished_list_size % (len(langs) * 100)
     wished_list_size += addition
     pages_per_lang = int(wished_list_size/len(langs)/100)
